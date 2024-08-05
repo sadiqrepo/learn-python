fe7606b650999d62139eac9a6b67ab86dc82ff59
sadiqrepo <sadiq.n11@gmail.com>
2024-08-05 08:09:26 +0530

    day 1

---
+++ day1_task/countChars.py
@@ -0,0 +1,5 @@
+#print(len(input()))
+
+name = input("What is your name?\n")
+length = len(name)
+print(length)
---
+++ day1_task/getInput.py
@@ -0,0 +1,5 @@
+print("Hello " + input("What is your name?"))
+
+#time_until_midnight = "5"
+#print("There are "+time_until_midnight+" hours until midnight")
+
